5c6bdd4a09f1e78b5015411f1719c28b2c87d915
scarletkeqing <qscarlet@umich.edu>
2023-09-19 13:56:39 -0400

    final edits

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -3,14 +3,8 @@ class Rectangle():
         self.width = width
         self.height = height
 
-    # Create the constructor "__init__" method
-    # Arguments: width (an ingeter), height (an integer)
-    # 
-    # It sets an instance variable, "width" to the passed argument, width
-    # It sets an instance variable, "height" to the passed argument, height
-
     def __str__(self):
-        return "A rectangle with width" + str(self.width) + "and height" + str(self.height)
+        return "A rectangle with width " + str(self.width) + " and height " + str(self.height)
     
     def verify_input(self):
         if self.width and self.height > 0:
@@ -20,26 +14,17 @@ class Rectangle():
 
     def area (self):
         self.verify_input()
-        if self.verify_input == False:
+        if self.verify_input == True:
             return self.width * self.height
         else:
             return "Invalid input"
         
     def perimeter (self):
         self.verify_input()
-        if self.verify_input == False:
+        if self.verify_input == True:
             return self.width + self.height + self.width + self.height
         else:
             return "Invalid input"
-
-
-
-
-    
-
-
-
-    
 
 
 def main():
